f7c72f3fb3cdff0650b23e84657ffc0c3506fcdc
keletso <keletsomonyamane.dev@gmail.com>
2026-09-08 20:27:09 +0200

    intitial project structure

---
+++ src/main/java/com/eventforge/eventforge_api/EventforgeApiApplication.java
@@ -0,0 +1,13 @@
+package com.eventforge.eventforge_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventforgeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventforgeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eventforge/eventforge_api/EventforgeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventforge.eventforge_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventforgeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
