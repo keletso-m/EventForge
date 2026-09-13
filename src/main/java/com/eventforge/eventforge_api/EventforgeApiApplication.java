ca5545fcb3b3776b7d3bca7b6d8810c53e8ad52e
keletso <keletsomonyamane.dev@gmail.com>
2026-09-13 19:28:28 +0200

    feat: add worker and enable scheduling

--- src/main/java/com/eventforge/eventforge_api/EventforgeApiApplication.java
+++ src/main/java/com/eventforge/eventforge_api/EventforgeApiApplication.java
@@ -2,12 +2,12 @@ package com.eventforge.eventforge_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class EventforgeApiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(EventforgeApiApplication.class, args);
 	}
-
 }
