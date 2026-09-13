f47d42315d921859c70e0cf3048f872ae19965df
keletso <keletsomonyamane.dev@gmail.com>
2026-09-13 17:39:37 +0200

    chore: add dependencies

--- src/main/java/com/eventforge/eventforge_api/config/SqsConfig.java
+++ src/main/java/com/eventforge/eventforge_api/config/SqsConfig.java
@@ -1,4 +1,10 @@
 package com.eventforge.eventforge_api.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.sqs.SqsClient;
 
 public class SqsConfig {
 }
