59f0829b25206a68bc7e034397c7c4a79b30cbcf
keletso <keletsomonyamane.dev@gmail.com>
2026-09-13 19:14:26 +0200

    chore: add new packages and depenencies

--- src/main/java/com/eventforge/eventforge_api/worker/EventWorker.java
+++ src/main/java/com/eventforge/eventforge_api/worker/EventWorker.java
@@ -1,4 +1,13 @@
 package com.eventforge.eventforge_api.worker;
+import com.eventforge.eventforge_api.event.Event;
+import com.eventforge.eventforge_api.event.EventRepository;
+import com.eventforge.eventforge_api.event.EventStatus;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import software.amazon.awssdk.services.sqs.SqsClient;
+import software.amazon.awssdk.services.sqs.model.DeleteMessageRequest;
+import software.amazon.awssdk.services.sqs.model.Message;
+import software.amazon.awssdk.services.sqs.model.ReceiveMessageRequest;
 
 public class EventWorker {
 }
