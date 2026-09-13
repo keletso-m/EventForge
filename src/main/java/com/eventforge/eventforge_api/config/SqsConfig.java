721d205ccd1acc2ca1162210f24d36b8a5d34e07
keletso <keletsomonyamane.dev@gmail.com>
2026-09-13 17:41:30 +0200

    feat: add SQS client bean and publish on event creation

--- src/main/java/com/eventforge/eventforge_api/config/SqsConfig.java
+++ src/main/java/com/eventforge/eventforge_api/config/SqsConfig.java
@@ -6,5 +6,21 @@ import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.sqs.SqsClient;
 
+import java.net.URI;
+
+@Configuration
 public class SqsConfig {
+
+    @Bean
+    public SqsClient sqsClient() {
+        return SqsClient.builder()
+                .endpointOverride(URI.create("http://localhost:4566"))
+                .region(Region.US_EAST_1)
+                .credentialsProvider(
+                        StaticCredentialsProvider.create(
+                                AwsBasicCredentials.create("test", "test")
+                        )
+                )
+                .build();
+    }
 }
